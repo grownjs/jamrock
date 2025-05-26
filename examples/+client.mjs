df7f4518cdb7d97929ac7bad7ddcf835c4892fdc
Alvaro Cabrera <pateketrueke@gmail.com>
2025-05-26 04:35:08 -0600

    wip: +client scripts

---
+++ examples/+client.mjs
@@ -0,0 +1,14 @@
+// import { actions } from 'jamrock:browser';
+
+export function click(node) {
+  console.log('[HANDLE]', node);
+  async function onClick() {
+    // FIXME: probably these should be a map, with all exported
+    // actions from the render tree... so we can prevalidate calls
+    // and such!
+    const result = await actions.callme();
+    console.log({ result });
+  }
+  node.addEventListener('click', onClick);
+  return () => node.removeEventListener('click', onClick);
+}
