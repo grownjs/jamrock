e73ca5f8465fa36a02cac51fc6a7f2e52c3d6573
Alvaro Cabrera <pateketrueke@gmail.com>
2025-11-20 23:19:23 -0600

    installs

---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+rm -rf $HOME/.jamrock
+mkdir -p $HOME/.jamrock
+
+echo "■ Downloading package..."
+
+if [[ $1 == "-" ]]; then
+  rm -f jamrock-*.tgz && npm pack
+  curl -s file://$PWD/jamrock-0.0.0.tgz | tar zx -C $HOME/.jamrock/
+else
+  curl -s https://jamrock.dev/jamrock-0.0.0.tgz | tar zx -C $HOME/.jamrock/
+fi
+
+if [[ ! -d $HOME/.jamrock/package ]]; then
+  echo "The package could not be downloaded!"
+  exit 1
+fi
+
+echo "■ Installing binary..."
+
+CLI=$HOME/.local/bin/jamrock
+RUNTIME=node
+
+ask_runtime() {
+  OPTIONS=(node deno bun)
+  PS3="Please choose a runtime: "
+
+  select choice in "${OPTIONS[@]}"; do
+    case $choice in
+      node)
+        break
+        ;;
+      deno)
+        break
+        ;;
+      bun)
+        break
+        ;;
+      *)
+        echo "Invalid option. Please try again."
+        ;;
+    esac
+  done
+
+  RUNTIME=$choice
+}
+
+ask_runtime
+
+cat << EOF > $CLI
+#!/bin/bash
+$HOME/.jamrock/package/bin/$RUNTIME \$*
+EOF
+
+chmod +x $CLI
+
+echo "■ Done, now grab a beer!"
