32101af660315fd0d368379829d10857501db3a5
Alvaro Cabrera <pateketrueke@gmail.com>
2026-04-17 15:55:16 -0600

    fix: initial RPC E2E test page structure, fully working

--- tests/e2e/cases/05_rpc.test.js
+++ tests/e2e/cases/05_rpc.test.js
@@ -7,6 +7,18 @@ import { $ } from '../selectors';
 fixture`RPC async functions`
   .page`http://localhost:3000/rpc-async`;
 
-test('should render the home page', async t => {
-  await t.expect($('body').exists).ok();
+test('should render the RPC page', async t => {
+  await t.expect($('h1').textContent).contains('RPC Test');
+});
+
+test('should have increment button', async t => {
+  await t.expect($('@increment').exists).ok();
+});
+
+test('should have decrement button', async t => {
+  await t.expect($('@decrement').exists).ok();
+});
+
+test('should have reset button', async t => {
+  await t.expect($('@reset').exists).ok();
 });
