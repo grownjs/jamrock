17ab5b88df8e96b63d1562475a8278723f6bfe6b
Alvaro Cabrera <pateketrueke@gmail.com>
2026-04-18 00:04:04 -0600

    test: add E2E RPC click tests for increment/decrement/reset

--- tests/e2e/cases/05_rpc.test.js
+++ tests/e2e/cases/05_rpc.test.js
@@ -5,7 +5,10 @@ import { $ } from '../selectors';
 /* global fixture, test */
 
 fixture`RPC async functions`
-  .page`http://localhost:3000/rpc-async`;
+  .page`http://localhost:8080/rpc-async`
+  .before(async () => {
+    await new Promise(r => setTimeout(r, 1000));
+  });
 
 test('should render the RPC page', async t => {
   await t.expect($('h1').textContent).contains('RPC Test');
@@ -21,4 +24,28 @@ test('should have decrement button', async t => {
 
 test('should have reset button', async t => {
   await t.expect($('@reset').exists).ok();
+});
+
+test('should increment server count on click', async t => {
+  await t.click($('@increment'));
+  await t.wait(1000);
+  await t.expect($('@server-count').exists).ok();
+});
+
+test('should decrement server count on click', async t => {
+  await t.click($('@increment'));
+  await t.wait(500);
+  await t.click($('@decrement'));
+  await t.wait(500);
+  const count = await $('@server-count').textContent;
+  await t.expect(count).contains('0');
+});
+
+test('should reset server count on click', async t => {
+  await t.click($('@increment'));
+  await t.wait(100);
+  await t.click($('@reset'));
+  await t.wait(100);
+  const countAfterReset = await $('@server-count').textContent;
+  await t.expect(countAfterReset).contains('0');
 });
